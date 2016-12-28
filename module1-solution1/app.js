fa5d34e11c0871dd3b565d80ee8b715f37112f0a
vinaymandala <vinaymandala@yahoo.com>
2016-12-28 17:53:15 +0100

    Updated code for the bug reported

--- module1-solution1/app.js
+++ module1-solution1/app.js
@@ -1,4 +1,4 @@
-/** MINIFIED 
+/** MINIFIED
 
 !function(){"use strict";function n(n){n.checkLunchItems=function(){var e=",",c=0,t=n.lunchItem;return null==t?void(n.displayMsg="Please enter data first!"):(c=t.split(e).length,void(3>=c?n.displayMsg="Enjoy!":n.displayMsg="Too Much!"))}}angular.module("LunchCheck",[]).controller("LunchCheckController",n),n.$inject=["$scope"]}();
 
@@ -18,18 +18,26 @@
      var lunchItemsLength = 0;
      var lunchMenuList =  $scope.lunchItem;
 
-     if(lunchMenuList == null){
+     alert(lunchMenuList);
+
+     if(lunchMenuList == null || lunchMenuList == ''){
        $scope.displayMsg = "Please enter data first!";
        return;
      } else {
       lunchItemsLength = lunchMenuList.split(separator).length;
      }
 
-     if (lunchItemsLength <= 3){
+     alert(lunchItemsLength);
+
+     if (lunchItemsLength == 0){
+       $scope.displayMsg = "Please enter data first!";
+     } else if (lunchItemsLength <= 3){
        $scope.displayMsg = "Enjoy!";
      } else {
        $scope.displayMsg = "Too Much!";
      }
+
+     lunchItemsLength = 0;
 
    };
  }
